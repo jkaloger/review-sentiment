3d5f21ee8b9389561b1a062a07869398e99c9a20
jkaloger <jkaloger@users.noreply.github.com>
2017-06-28 22:02:02 +1000

    dataset load

--- parser.py
+++ parser.py
@@ -4,12 +4,14 @@ parser for review polarity data
 from https://www.cs.cornell.edu/people/pabo/movie-review-data/
 '''
 
+import os
+
 def parse(path, pos, neg):
     out = []
     poles = [pos, neg]
     for pole in poles:
         for filename in os.listdir(path + pole):
-            f = open(filename, 'r')
+            f = open(path + pole + '/' + filename, 'r')
             out.append({"sentiment": pole, "text": f.read()})
 
     return out
--- reviewsentiment.py
+++ reviewsentiment.py
@@ -3,3 +3,12 @@ sentiment analysis for movie reviews
 '''
 
 import parser
+from random import shuffle
+from math import floor
+
+def load_data(path):
+	dataset = parser.parse(path, "pos", "neg")
+	shuffle(dataset)
+	shuffle(dataset)
+	# return (dev, test)
+	return (dataset[:floor(len(dataset)*0.7)], dataset[floor(len(dataset)*0.7) + 1:])
