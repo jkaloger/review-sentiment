d3c5f45dd2575f364a249bc1f0d12493593dc96a
jkaloger <jkaloger@users.noreply.github.com>
2017-07-04 21:41:14 +1000

    svm ~81% acc

--- reviewsentiment.py
+++ reviewsentiment.py
@@ -6,6 +6,7 @@ import parser
 from random import shuffle
 from math import floor
 from sklearn.naive_bayes import GaussianNB
+from sklearn.svm import LinearSVC
 from sklearn.feature_extraction.text import TfidfVectorizer
 
 def load_data(path):
@@ -24,10 +25,24 @@ def acc(predicted, real):
 	return i/len(predicted)
 
 
+def svm_predict():
+	svm = LinearSVC()
+	svm.fit(X.toarray(), [x["sentiment"] for x in dataset[0]])
+
+	labels = svm.predict(vectorizer.transform([x["text"] for x in dataset[1]]).toarray())
+
+	print(acc(labels, [x["sentiment"] for x in dataset[1]]))
+
+
+def nb_predict():
+	nb = GaussianNB()
+	nb.fit(X.toarray(), [x["sentiment"] for x in dataset[0]])
+	labels = nb.predict(vectorizer.transform([x["text"] for x in dataset[1]]).toarray())
+	print(acc(labels, [x["sentiment"] for x in dataset[1]]))
+
+
 dataset = load_data("data/")
 vectorizer = TfidfVectorizer(analyzer="word", ngram_range=(2,2))
 X = vectorizer.fit_transform([x["text"] for x in dataset[0]])
-nb = GaussianNB()
-nb.fit(X.toarray(), [x["sentiment"] for x in dataset[0]])
-labels = nb.predict(vectorizer.transform([x["text"] for x in dataset[1]]).toarray())
-print(acc(labels, [x["sentiment"] for x in dataset[1]]))
+
+svm_predict()
